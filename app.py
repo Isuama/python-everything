c7fd0781249c709f847a41438500f6e96c7a59d6
isuama <isuama.amarathunga@gmail.com>
2025-07-05 06:05:15 +0530

    get all servants working

--- app.py
+++ app.py
@@ -1,9 +1,10 @@
 from flask import Flask
+from flask_restx import Api
 from application.routes.servant_routes import servant_bp
 
 def create_app():
     app = Flask(__name__)
     app.secret_key = "your-secret-key"
 
-    app.register_blueprint(servant_bp)#,url_prefix='/api')
+    app.register_blueprint(servant_bp)
     return app
